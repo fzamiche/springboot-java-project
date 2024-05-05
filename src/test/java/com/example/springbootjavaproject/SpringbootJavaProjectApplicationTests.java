09891cb56599ecf8540fd35d291ecafa67ae0bd6
fzamiche <zamiche.fatah@gmail.com>
2024-05-05 16:00:06 +0200

    Initial commit

---
+++ src/test/java/com/example/springbootjavaproject/SpringbootJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
